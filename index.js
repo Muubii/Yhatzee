00444974990992305da3b76b77a6b0cdcd31b797
Muubi <siu7279@gmail.com>
2023-12-08 11:54:46 +0100

    Roll dice button en popup
    Goed ging
    -Weten wat ik wil doen
    -Roll ging makkelijk
    Lastig
    hoe ik het ging laten zien in html site

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ let scores = {
     fives: 0,
     sixes: 0
 };
+
+// rollen hoeveel je krijgt
+function rollDice() {
+    if (rollsLeft > 0) {
+        diceValues = [];
+        let diceContainer = document.getElementById('dice-container');
+        diceContainer.innerHTML = '';
+
+        for (let i = 0; i < 5; i++) {
+            let diceValue = Math.floor(Math.random() * 6) + 1;
+            diceValues.push(diceValue);
+
+            let diceElement = document.createElement('div');
+            diceElement.className = 'dice';
+            diceElement.textContent = diceValue;
+            diceElement.setAttribute('data-index', i);
+            diceElement.onclick = toggleHold;
+            diceContainer.appendChild(diceElement);
+        }
+
+        rollsLeft--;
+
+        if (rollsLeft === 0) {
+            calculateScore();
+        }
+    }
+}
+
+function toggleHold(event) {
+    let index = event.target.getAttribute('data-index');
+    let diceElement = document.querySelector('[data-index="' + index + '"]');
+
+    if (diceElement.classList.contains('held')) {
+        diceElement.classList.remove('held');
+    } else {
+        diceElement.classList.add('held');
+    }
+}
